2aaa44e6fbbf06dd32bb630a87b18d0efebb0467
최준영 <cjy4780@naver.com>
2025-11-23 14:30:06 +0900

    Refactor: CategoryItems 객체 분리 리팩토링

---
+++ app/src/main/java/org/speed/realestate/constant/CategoryItems.kt
@@ -0,0 +1,9 @@
+package org.speed.realestate.constant
+
+object CategoryItems {
+    const val NAME = "아파트명"
+    const val DONG = "동"
+    const val TYPE = "타입"
+    const val OPTION = "옵션"
+    const val TRADETYPE = "거래유형"
+}
