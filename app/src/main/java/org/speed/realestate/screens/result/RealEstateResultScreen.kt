f6a89fa020e6534c66dea02ea7b3e91c046054be
최준영 <cjy4780@naver.com>
2025-11-23 19:00:57 +0900

    Feat: 매물 결과페이지 클릭 기능 구현

--- app/src/main/java/org/speed/realestate/screens/result/RealEstateResultScreen.kt
+++ app/src/main/java/org/speed/realestate/screens/result/RealEstateResultScreen.kt
@@ -18,6 +18,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -25,20 +27,34 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.speed.realestate.RealEstateDetail
+import org.speed.realestate.UpdateRealEstateSheet
 import org.speed.realestate.data.RealEstate
 import org.speed.realestate.ui.theme.backgroundColor
+import org.speed.realestate.ui.theme.detailPriceTextColor
+import org.speed.realestate.ui.theme.detailRedColor
+import org.speed.realestate.ui.theme.priceBoxColor
+import org.speed.realestate.ui.theme.simpleCardBoxColor
+import org.speed.realestate.ui.theme.simpleCardDotColor
+import org.speed.realestate.ui.theme.simpleCardIconColor
+import org.speed.realestate.ui.theme.simpleCardTradeTypeColor
+import org.speed.realestate.ui.theme.simpleCardTypeColor
 import org.speed.realestate.ui.viewmodel.RealEstateViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,6 +65,11 @@ fun RealEstateResultScreen(
     onBackClick: () -> Unit,
 ) {
     val realEstates by viewModel.filteredRealEstates.collectAsState()
+    var showRealEstateDetail by remember { mutableStateOf(false) }
+    var selectedRealEstate: RealEstate? by remember { mutableStateOf(null) }
+    var showDeleteConfirmAlertDialog by remember { mutableStateOf(false) }
+    var showUpdateRealEstate by remember { mutableStateOf(false) }
+    val tags by viewModel.tags.collectAsState()
 
     Scaffold(
         topBar = { RealEstateListPageTopBar(onBackClick = onBackClick) },
@@ -80,11 +101,83 @@ fun RealEstateResultScreen(
                 items(realEstates.size) { realEstate ->
                     RealEstateSimpleCard(
                         realEstate = realEstates[realEstate],
-                        onCardClick = {}
+                        onCardClick = {
+                            selectedRealEstate = realEstates[realEstate]
+                            showRealEstateDetail = true
+                        }
                     )
                 }
             }
         }
+    }
+
+    if (showRealEstateDetail) {
+        RealEstateDetail(
+            realEstate = selectedRealEstate!!,
+            onDismiss = {
+                showRealEstateDetail = false
+            },
+            onDelete = {
+                showDeleteConfirmAlertDialog = true
+            },
+            onUpdate = {
+                showUpdateRealEstate = true
+            },
+        )
+    }
+
+    if (showDeleteConfirmAlertDialog) {
+        AlertDialog(
+            onDismissRequest = { showDeleteConfirmAlertDialog = false },
+            title = { Text("삭제 확인") },
+            text = { Text("정말로 이 매물을 삭제하시겠습니까?") },
+            confirmButton = {
+                OutlinedButton(
+                    onClick = {
+                        viewModel.delete(selectedRealEstate!!)
+                        showDeleteConfirmAlertDialog = false
+                        showRealEstateDetail = false
+                    },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White,
+                        contentColor = detailRedColor
+                    )
+                ) {
+                    Text("삭제")
+                }
+            },
+            dismissButton = {
+                OutlinedButton(
+                    onClick = { showDeleteConfirmAlertDialog = false },
+                    shape = RoundedCornerShape(8.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.White,
+                        contentColor = detailRedColor
+                    )
+                )
+                {
+                    Text("취소")
+                }
+            },
+            containerColor = Color.White,
+            shape = RoundedCornerShape(8.dp)
+        )
+    }
+
+    if (showUpdateRealEstate) {
+        UpdateRealEstateSheet(
+            tags = tags,
+            realEstate = selectedRealEstate!!,
+            onSubmit = {
+                viewModel.update(it)
+                selectedRealEstate = it
+                showUpdateRealEstate = false
+            },
+            onDismiss = {
+                showUpdateRealEstate = false
+            },
+        )
     }
 }
 
@@ -152,13 +245,13 @@ fun RealEstateSimpleCard(
 
                     Box(
                         modifier = Modifier
-                            .background(Color(0xFFE3F2FD), RoundedCornerShape(8.dp))
+                            .background(simpleCardBoxColor, RoundedCornerShape(8.dp))
                             .padding(horizontal = 10.dp, vertical = 4.dp)
                     ) {
                         Text(
                             text = realEstate.tradeType,
                             fontSize = 17.sp,
-                            color = Color(0xFF1565C0)
+                            color = simpleCardTradeTypeColor
                         )
                     }
                 }
@@ -167,7 +260,7 @@ fun RealEstateSimpleCard(
                     Text(
                         text = "${realEstate.type} 타입",
                         fontSize = 15.sp,
-                        color = Color(0xFF616161)
+                        color = simpleCardTypeColor
                     )
                 }
             }
@@ -178,12 +271,12 @@ fun RealEstateSimpleCard(
                 Icon(
                     imageVector = Icons.Default.LocationOn,
                     contentDescription = null,
-                    tint = Color(0xFF455A64)
+                    tint = simpleCardIconColor
                 )
                 Spacer(modifier = Modifier.width(6.dp))
-                Text(text = "${realEstate.dong}동", fontSize = 15.sp, color = Color(0xFF455A64))
-                Text(text = "  •  ", fontSize = 15.sp, color = Color(0xFFB0BEC5))
-                Text(text = "${realEstate.ho}호", fontSize = 15.sp, color = Color(0xFF455A64))
+                Text(text = "${realEstate.dong}동", fontSize = 15.sp, color = simpleCardIconColor)
+                Text(text = "  •  ", fontSize = 15.sp, color = simpleCardDotColor)
+                Text(text = "${realEstate.ho}호", fontSize = 15.sp, color = simpleCardIconColor)
             }
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -191,13 +284,13 @@ fun RealEstateSimpleCard(
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color(0xFFFFF1F1), RoundedCornerShape(8.dp))
+                    .background(priceBoxColor, RoundedCornerShape(8.dp))
                     .padding(vertical = 10.dp, horizontal = 15.dp)
             ) {
                 Text(
                     text = realEstate.price ?: "가격정보없음",
                     fontSize = 20.sp,
-                    color = Color(0xFFB71C1C)
+                    color = detailPriceTextColor
                 )
 
             }
