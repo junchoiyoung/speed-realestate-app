b2150ebfbd36588084721f06276667917b419cb1
최준영 <cjy4780@naver.com>
2025-11-23 18:56:22 +0900

    Feat: 컬러 변수 추가

--- app/src/main/java/org/speed/realestate/ui/theme/Color.kt
+++ app/src/main/java/org/speed/realestate/ui/theme/Color.kt
@@ -11,3 +11,20 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val backgroundColor = Color(0xFFF7F7F7)
+val selectedContainerColor = Color(0xFFE8F0FF)
+val selectedContentColor = Color(0xFF2962FF)
+
+val contentColor = Color(0xFF333333)
+val borderColor = Color(0xFFD9D9D9)
+
+val detailBackgroundColor = Color(0xFFD3D3D3)
+val detailPriceTextColor = Color(0xFFB71C1C)
+val detailRedColor = Color(0xFFE53935)
+
+
+val simpleCardTradeTypeColor = Color(0xFF1565C0)
+val simpleCardBoxColor = Color(0xFFE3F2FD)
+val simpleCardTypeColor = Color(0xFF616161)
+val simpleCardIconColor = Color(0xFF455A64)
+val priceBoxColor = Color(0xFFFFEBEB)
+val simpleCardDotColor = Color(0xFFB0BEC5)
